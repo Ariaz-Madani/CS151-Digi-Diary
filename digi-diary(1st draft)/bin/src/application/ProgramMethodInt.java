4acc432ff5d2df8f3ee1f81a3dbf8d43dc72c184
AW <69340672+BigOlSheesh@users.noreply.github.com>
2023-07-27 20:27:37 -0700

    Update ProgramMethodInt.java

--- digi-diary(1st draft)/bin/src/application/ProgramMethodInt.java
+++ digi-diary(1st draft)/bin/src/application/ProgramMethodInt.java
@@ -1,36 +1,17 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
 
-import javafx.application.Platform;
+import javafx.animation.FadeTransition;
 import javafx.beans.value.ObservableValue;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -38,6 +19,9 @@ import javafx.scene.media.MediaView;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
+/**
+ * Interface holding methods for creating/displaying GUI objects/nodes.
+ */
 public interface ProgramMethodInt extends ProgramValueInt{
 	
 	/**
@@ -109,6 +93,19 @@ public interface ProgramMethodInt extends ProgramValueInt{
 		MediaPlayer mp = new MediaPlayer(new Media(new File(file).toURI().toString()));
 		mp.setVolume(volume);
 		return mp;
+	}
+ 	
+ 	/**
+	 * Method createTransition will make the specified node fade in during a select amount of time.
+	 * @param duration		time for fade to fill in
+	 * @param node			node to fade in
+	 * @param opacity		opacity for fade to fill to
+	 */
+	public default void createTransition(Duration duration, Node node, double opacity){
+		FadeTransition fade = new FadeTransition(duration, node);
+		fade.setFromValue(0.0);
+		fade.setToValue(opacity);
+		fade.play();
 	}
  	
  	/**
@@ -283,7 +280,6 @@ public interface ProgramMethodInt extends ProgramValueInt{
 	//add to root unimplemented method
 	public void add(Object obj);
 	
-
 	/**
 	 * Method assigns values to set up VBox that is defined in interface
 	 * @param vBox				the container to be edited
@@ -312,169 +308,24 @@ public interface ProgramMethodInt extends ProgramValueInt{
 			vBox.prefWidthProperty().bind(x);
 		if (y != null) 
 			vBox.prefHeightProperty().bind(y);	
-}
-
-	/**
-	 * Method displayLocalDateTime is used to either display a continiously updating date, time, or both on the GUI application.
-	 * @param timeAndDate		textField that will list both time and/or date
-	 * @param dateAndOrTime		"date" = just display date, "time" = just display time, "both" = display both
-	 */
-	public default void displaylocalDateTime(TextField timeAndDate, String dateAndOrTime) {
-		boolean off = false;		//boolean variable to ensure thread keeps running indefinitely
-		Thread thread = new Thread(() -> {
-			SimpleDateFormat dateTime = null;
-			
-			if (dateAndOrTime.equals("date")) {							//if just want to display date
-				dateTime = new SimpleDateFormat("yyyy/MM/dd");
-			}
-			else if (dateAndOrTime.equals("time")) {					//if just want to display time
-				dateTime = new SimpleDateFormat("HH:mm:ss");
-			}
-			else if (dateAndOrTime.equals("both")) {					//if want to display both
-				dateTime = new SimpleDateFormat("yyyy/MM/dd     	HH:mm:ss");
-			}
-			
-			while (off == false) {		//the while loop that will keep updating the textfield; this method can be more specific in wanting label or textfield in its parameters but not for this proj
-				try {
-					Thread.sleep(1000);			//1000 milisecond convets to 1 second and will check/update after every second
-					
-				} catch(Exception e) {e.printStackTrace();}
-				
-				String currentDateTime = dateTime.format(new Date());		//get the current update date/time
-				
-				Platform.runLater(() -> {								//runs as a separate task after running main program
-					timeAndDate.setText(currentDateTime);				//update the specified field
-				});
-			}
-		});
-		thread.setDaemon(true);		//this will make sure the thread closes when application closes by user
-		thread.start();				//start the thread/run process
-	}
-	
-	
-	/**
-	 * Method clearFile will reset a specified file's content
-	 * @param file				the file to clear
-	 */
-	public default void clearFile(File file)
-	{ 
-	    try{
-	    FileWriter fw = new FileWriter(file, false);
-	    PrintWriter pw = new PrintWriter(fw, false);
-	    pw.flush();
-	    pw.close();
-	    fw.close();
-	    }
-	    catch(Exception exception){
-	        System.out.println(exception);
-	    }
 	}
 	
 	/**
-	 * Method getFileContent will get content from specified file in the form of an arraylist
-	 * @param file				the file to retrieve content from
-	 * @return content			the content of the file
+	 * Method addMenu will preset numbers in specified combobox while also limiting how many menu items are displayed.
+	 * @param box			the combobox to add numbers to
+	 * @param numbers		the numbers to be added from 0
+	 * @param visibleTotal	the total number of visible menu items displayed at a time
 	 */
-	public default String getFileContent(File file) {
-		String content = "";
-		
-		try {
-			FileReader fr = new FileReader(file);
-			BufferedReader br = new BufferedReader(fr);
-			
-			String line = br.readLine();
-			int tick = 0;
-			while (line != null) {		// while line is not null = content remaining = read all till the end of file
-				if (!line.isEmpty()) {		//empty line = indent twice
-					if (tick == 0) {
-						content = line;
-						tick = 1;
-					}
-					else {
-						content = content + "\n" + line;
-					}
-				}
-				else {						//not empty line but new line = indent once
-					content = content + "\n\n";
-				}
-				line = br.readLine();		//read next line
-			}
-			
-			br.close();
-			fr.close();
-		} 
-		catch (IOException e) {e.printStackTrace();}
-		
-		return content;
-	}
-	
-	/**
-	 * Method findFile will return a list of file entries who contain the specified line of text user wants to find.
-	 * @param str		the string of text that user wants found
-	 * @param mode		specifies if files are being searching from deleted folder or saved folder
-	 * @return list		the list of entires that fit the search criteria
-	 */
-	public default ArrayList<String> findFile(String text, String mode) {
-		ArrayList<String> list = new ArrayList<>();
-		
-		//get a list of ALL entries of specified folder
-		if (mode.equals("deleted")) {		//recovery mode
-			try {
-				FileReader fr = new FileReader(deletedFile);
-				BufferedReader br = new BufferedReader(fr);
-				
-				String line = br.readLine();
-				while (line != null) {
-					list.add("src/fileDB/deletedFiles/" + line);		//list of paths to all deleted files
-					line = br.readLine();
-				}
-				
-				br.close();
-				fr.close();
-			}
-			catch(IOException e) {e.printStackTrace();}
-		}
-		else {		//search for old entry mode
-			try {
-				FileReader fr = new FileReader(savedFile);
-				BufferedReader br = new BufferedReader(fr);
-				
-				String line = br.readLine();
-				while (line != null) {
-					list.add("src/fileDB/savedFiles/" + line);			//list of paths to all saved files
-					line = br.readLine();
-				}
-				
-				br.close();
-				fr.close();
-			}
-			catch(IOException e) {e.printStackTrace();}
-		}
-		
-		//run through each and every file in specified folder/list and delete those that don't contain user's desired text
-		for (int i = 0; i < list.size(); i++) {
-			String str = list.get(i);
-			File file = new File(str);		//make a file with the path
-			
-			if (mode.equals("deleted")) {
-				str = str.replace("src/fileDB/deletedFiles/", "");
+	public default void addMenu(ComboBox<String> box, int numbers, int visibleTotal) {
+		for (int i = numbers; i > -1; i--) {
+			if (i < 10) {
+				box.getItems().add("0" + String.valueOf(i));
 			}
 			else {
-				str = str.replace("src/fileDB/savedFiles/", "");
-			}
-			list.remove(i);
-			list.add(i, str);		//convert back to just the file name; this is the format that the dropBoxMenu will read in
-			
-			if (!file.toString().contains("null") && str.contains(text)) {
-				str = str + getFileContent(file);		//get a string of the title + the file's content
-			}
-			else {				//remove entry from the list if it doesn't contain user's specified text
-				list.remove(i);
-				i--;
+				box.getItems().add(String.valueOf(i));
 			}
 		}
-		
-		return list;
+		box.setVisibleRowCount(visibleTotal);
 	}
 }
 
